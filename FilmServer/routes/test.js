3f75c18b8a196f4803a7e3a850dcb375d6c26427
Long <731986844@qq.com>
2017-05-23 20:22:44 +0800

    添加一些开发中的功能，包括hash、cookie、jwt的token

--- FilmServer/routes/test.js
+++ FilmServer/routes/test.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cookie = require('cookie');
+var jwt = require('jwt-simple');
+var jwtauth = require('../middleWare/jwtauth.js');
+
+var moment = require('moment');
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const crypto = require('crypto');
 
@@ -11,6 +20,68 @@ router.get('/', function(req, res, next) {
   hash.update(data);
   console.log(hash.digest('hex'));
   res.send('This is a test.');
+});
+
+// 返回hash加密的结果
+router.post('/', function(req, res, next) {
+  var data = req.query.data;
+  const hash = crypto.createHash('sha256');
+  hash.update(data);
+  var result = hash.digest('hex');
+  console.log(result);
+  res.send(result);
+});
+
+/* test for cookie */
+router.get('/cookie', function(req, res, next) {
+  console.log('Cookie: ', req.cookies);
+  
+  var cookieName = req.cookies.cookieName;
+  // 如果没有cookie，则新建一个并返回
+  if (cookieName === undefined) {
+    var randomNumber = Math.random().toString();
+    console.log(randomNumber);
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    console.log(randomNumber);
+    res.cookie('cookieName', randomNumber, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7 // 1 week
+    });
+    console.log('set cookie');
+  } else {
+    console.log('cookie exists');
+  }  
+  
+  res.send('Hello cookie.');
+});
+
+/* test for token */
+router.post('/jwt', [jsonParser, jwtauth], function(req, res, next) {
+  var user = {};
+  user.name = req.query.name;
+  var token = req.name;
+  
+  if (token) {
+    res.send('Hello token.' + token);
+  } else {
+    console.log(user);
+    // 超时的时限
+    var expires = moment().add(1, 'minute').valueOf();
+    console.log(expires);
+
+    // 生成token并加密
+    var token = jwt.encode({
+      iss: user.name,
+      exp: expires
+    }, 'FILM_SECRET_STRING');
+    
+    res.json({
+      token: token,
+      expires: expires,
+      user: JSON.stringify(user)
+    });
+    
+  }
 });
 
 module.exports = router;
