a91060e8db8796a139d77363e41540f079d33928
Long <731986844@qq.com>
2017-05-31 14:48:19 +0800

    添加images.js，用于传输图片

---
+++ FilmServer/routes/images.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+/* GET image by name. */
+router.get('/', function(req, res, next) {
+  var imageName = req.query.imageName;
+//  console.log(imageName);
+  if (imageName !== undefined) {
+    var imagePath = '../oriData/image/' + imageName;
+    res.sendFile(path.resolve(imagePath), function (err) {
+      if (err) {
+        console.log('err');
+        next(err);
+      } else {
+        console.log('Send: ', imageName);
+      }
+    });
+  } else {
+    res.send('Not imagesName!');
+  }
+});
+
+module.exports = router;
